ce24982fb89fd15c5f45817a2ff78b653bfb67c2
Jose Cordoba <jose.ma.97@hotmail.com>
2021-05-19 20:17:15 -0500

    se versiona carpeta proyecto angular sencillo con un componente

--- Restaurante-Semillero/src/main/java/com/heinsohn/semillero/RestauranteSemilleroApplication.java
+++ Restaurante-Semillero/src/main/java/com/heinsohn/semillero/RestauranteSemilleroApplication.java
@@ -2,12 +2,21 @@ package com.heinsohn.semillero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class RestauranteSemilleroApplication {
+@EnableDiscoveryClient
+public class RestauranteSemilleroUsuariosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestauranteSemilleroApplication.class, args);
+		SpringApplication.run(RestauranteSemilleroUsuariosApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
 }
